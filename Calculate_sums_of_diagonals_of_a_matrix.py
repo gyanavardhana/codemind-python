7c5e33c587a51a0affc99c892b1a91d560e3a93d
gyanavardhana <89439095+gyanavardhana@users.noreply.github.com>
2022-06-22 21:47:48 +0530

    mtrx=[] r=int(input()) c=r o=c for i in range(r):     j=list(map(int,input().split()))     mtrx.append(j) ##to print sum of diagnols su=0 ju=0 for i in range(r):     for j in range(c):         if i==j:             su=su+mtrx[i][j]         elif i+j==o-1:             ju=ju+mtrx[i][j] if r%2!=0:     ju=ju+mtrx[r//2][r//2]
    print("Principal Diagonal:",su,sep="")
    print("Secondary Diagonal:",ju,sep="")

---
+++ Calculate_sums_of_diagonals_of_a_matrix.py
@@ -0,0 +1,21 @@
+mtrx=[]
+r=int(input())
+c=r
+o=c
+for i in range(r):
+    j=list(map(int,input().split()))
+    mtrx.append(j)
+##to print sum of diagnols
+su=0
+ju=0
+for i in range(r):
+    for j in range(c):
+        if i==j:
+            su=su+mtrx[i][j]
+        elif i+j==o-1:
+            ju=ju+mtrx[i][j]
+if r%2!=0:
+    ju=ju+mtrx[r//2][r//2]
+    
+print("Principal Diagonal:",su,sep="")
+print("Secondary Diagonal:",ju,sep="")
