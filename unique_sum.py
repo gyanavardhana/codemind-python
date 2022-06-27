d3522752ac8b996ffaaa4bdfa6cbf9066e134ef7
gyanavardhana <89439095+gyanavardhana@users.noreply.github.com>
2022-06-28 00:43:14 +0530

    n=int(input()) l=list(map(int,input().split()))
    def lis(lis):
        s=[]
        for i in lis:
            if i not in s:
                s.append(i)
        return s
    a=[]
    s=lis(l)
    print(sum(s))

---
+++ unique_sum.py
@@ -0,0 +1,13 @@
+
+n=int(input())
+l=list(map(int,input().split()))
+
+def lis(lis):
+    s=[]
+    for i in lis:
+        if i not in s:
+            s.append(i)
+    return s
+a=[]
+s=lis(l)
+print(sum(s))
