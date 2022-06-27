610e5ffffce1d144fa9f93970930e22dfc3df85b
gyanavardhana <89439095+gyanavardhana@users.noreply.github.com>
2022-06-28 00:32:56 +0530

    def f(n):     c=0     for i in l:         if n==i:             c=c+1     return c n=int(input()) l=list(map(int,input().split())) b=int(input()) def lis(lis):     s=[]     for i in lis:         if i not in s:             s.append(i)     return s a=[] s=lis(l) for i in s:     if f(i)==b:         a.append(i) if len(a)==0:     print(-1) else:     print(*a)

---
+++ N_times_repeated_elements.py
@@ -0,0 +1,24 @@
+def f(n):
+    c=0
+    for i in l:
+        if n==i:
+            c=c+1
+    return c
+n=int(input())
+l=list(map(int,input().split()))
+b=int(input())
+def lis(lis):
+    s=[]
+    for i in lis:
+        if i not in s:
+            s.append(i)
+    return s
+a=[]
+s=lis(l)
+for i in s:
+    if f(i)==b:
+        a.append(i)
+if len(a)==0:
+    print(-1)
+else:
+    print(*a)
