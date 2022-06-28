7e882fa934a44a052d33b35ce59b470424023edd
gyanavardhana <89439095+gyanavardhana@users.noreply.github.com>
2022-06-28 14:07:30 +0530

    n=int(input()) l=list(map(int,input().split()))[:n] m=[] k=[] j=n//2 for i in range(j):     m.append(l[i]) for i in range(j,len(l)):     k.append(l[i]) print(abs(sum(k)-sum(m)))

---
+++ difference_between_sum_of_first_half_and_second_half_in_an_array.py
@@ -0,0 +1,10 @@
+n=int(input())
+l=list(map(int,input().split()))[:n]
+m=[]
+k=[]
+j=n//2
+for i in range(j):
+    m.append(l[i])
+for i in range(j,len(l)):
+    k.append(l[i])
+print(abs(sum(k)-sum(m)))
