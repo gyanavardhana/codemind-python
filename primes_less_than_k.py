e28a5ddb0e935b87007baff6b3d31f6cfe9775b9
gyanavardhana <89439095+gyanavardhana@users.noreply.github.com>
2022-06-28 14:30:01 +0530

    n=int(input()) k=list(map(int,input().split()))[:n] c=0 b=int(input()) def prime(n):     if n==1:         return False     for i in range(2,n):         if n%i==0:             return False             break     else:         return True for i in k:     if i<=b:         if prime(i)==True:             c+=1 print(c)

---
+++ primes_less_than_k.py
@@ -0,0 +1,18 @@
+n=int(input())
+k=list(map(int,input().split()))[:n]
+c=0
+b=int(input())
+def prime(n):
+    if n==1:
+        return False
+    for i in range(2,n):
+        if n%i==0:
+            return False
+            break
+    else:
+        return True
+for i in k:
+    if i<=b:
+        if prime(i)==True:
+            c+=1
+print(c)
