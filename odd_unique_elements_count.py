1d27e8ff48fa873bf7ebc20960bf4334fbec1400
gyanavardhana <89439095+gyanavardhana@users.noreply.github.com>
2022-06-28 12:33:11 +0530

    n=int(input()) l=list(map(int,input().split())) def s(lis):     s=[]     for i in l:         if i not in s:             s.append(i)     return s def f(n):     c=0     for i in l:         if n==i:             c=c+1     return c su=0 s=s(l) for i in s:     if i%2!=0:         su+=1 print(su)

---
+++ odd_unique_elements_count.py
@@ -0,0 +1,20 @@
+n=int(input())
+l=list(map(int,input().split()))
+def s(lis):
+    s=[]
+    for i in l:
+        if i not in s:
+            s.append(i)
+    return s
+def f(n):
+    c=0
+    for i in l:
+        if n==i:
+            c=c+1
+    return c
+su=0
+s=s(l)
+for i in s:
+    if i%2!=0:
+        su+=1
+print(su)
