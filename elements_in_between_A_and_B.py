f60308191041c8e3d84e219ecd8c52bcec2960e6
gyanavardhana <89439095+gyanavardhana@users.noreply.github.com>
2022-07-01 20:00:37 +0530

    n=int(input()) l=list(map(int,input().split()))[:n] a,b=map(int,input().split()) m=[] k=[] for i in range(len(l)):     if l[i]<a or l[i]>b:         m.append(l[i]) for i in l:     if i not in m:         k.append(i) if len(k)==0:     print(-1) else:     print(*(k))

--- elements_in_between_A_and_B.py
+++ elements_in_between_A_and_B.py
@@ -1,16 +1,15 @@
 n=int(input())
-l=list(map(int,input().split()))
+l=list(map(int,input().split()))[:n]
 a,b=map(int,input().split())
 m=[]
 k=[]
-for i in l:
-    if i<a or i>b:
-        m.append(i)
+for i in range(len(l)):
+    if l[i]<a or l[i]>b:
+        m.append(l[i])
 for i in l:
     if i not in m:
         k.append(i)
 if len(k)==0:
     print(-1)
 else:
-    
-    print(*k)
+    print(*(k))
