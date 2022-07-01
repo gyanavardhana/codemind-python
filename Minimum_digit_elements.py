25940b5351bb002febb08c54b427724872a6a799
gyanavardhana <89439095+gyanavardhana@users.noreply.github.com>
2022-07-01 21:55:37 +0530

    n=int(input()) l=list(map(int,input().split())) m=min(l) c=0 for i in l:     if len(str(i))==len(str(m)):         c+=1 print(c)

--- Minimum_digit_elements.py
+++ Minimum_digit_elements.py
@@ -1,9 +1,8 @@
 n=int(input())
 l=list(map(int,input().split()))
-k=len(str(min(l)))
+m=min(l)
 c=0
 for i in l:
-    if len(str(i))==k:
+    if len(str(i))==len(str(m)):
         c+=1
 print(c)
-    
