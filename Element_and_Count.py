0779130c152fcdaccb6e457c1870c6a97ae4832a
gyanavardhana <89439095+gyanavardhana@users.noreply.github.com>
2022-07-01 20:08:49 +0530

    n=int(input()) l=list(map(int,input().split()))[:n] def lis(lis):     s=[]     for i in lis:         if i not in s:             s.append(i)     return s s=lis(l) for i in s:     print(i,l.count(i),end=' ')

--- Element_and_Count.py
+++ Element_and_Count.py
@@ -1,17 +1,11 @@
 n=int(input())
-l=list(map(int,input().split()))
-def f(n):
-    c=0
-    for i in l:
-        if i==n:
-            c+=1
-    return c
-def s(lis):
+l=list(map(int,input().split()))[:n]
+def lis(lis):
     s=[]
-    for i in l:
+    for i in lis:
         if i not in s:
             s.append(i)
     return s
-s=s(l)
+s=lis(l)
 for i in s:
-    print(i,f(i),end=" ")
+    print(i,l.count(i),end=' ')
