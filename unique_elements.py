7aeea202664426711ec83375a729c6845b03636d
gyanavardhana <89439095+gyanavardhana@users.noreply.github.com>
2022-07-01 20:26:59 +0530

    n=int(input()) l=list(map(int,input().split()))[:n] def s(lis):     s=[]     for i in l:         if i not in s:             s.append(i)     return s print(*(s(l)))

--- unique_elements.py
+++ unique_elements.py
@@ -1,9 +1,9 @@
 n=int(input())
-l=list(map(int,input().split()))
-def f(l):
-    k=[]
+l=list(map(int,input().split()))[:n]
+def s(lis):
+    s=[]
     for i in l:
-        if i not in k:
-            k.append(i)
-    return k
-print(*(f(l)))
+        if i not in s:
+            s.append(i)
+    return s
+print(*(s(l)))
