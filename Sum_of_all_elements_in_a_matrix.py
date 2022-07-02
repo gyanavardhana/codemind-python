4c58a023b7557af028c233a162f5516b6526d9cb
gyanavardhana <89439095+gyanavardhana@users.noreply.github.com>
2022-07-02 23:41:32 +0530

    r,c=map(int,input().split()) mtrx=[] for i in range(r):     j=list(map(int,input().split()))     mtrx.append(j) su=0 for i in range(r):     for j in range(c):         su=su+mtrx[i][j] print(su)

---
+++ Sum_of_all_elements_in_a_matrix.py
@@ -0,0 +1,10 @@
+r,c=map(int,input().split())
+mtrx=[]
+for i in range(r):
+    j=list(map(int,input().split()))
+    mtrx.append(j)
+su=0
+for i in range(r):
+    for j in range(c):
+        su=su+mtrx[i][j]
+print(su)
