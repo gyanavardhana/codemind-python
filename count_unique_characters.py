c89ff536792b9d4ab2a9a1e90e0ad06358d5ef70
gyanavardhana <89439095+gyanavardhana@users.noreply.github.com>
2022-07-02 22:43:55 +0530

    s=input() a=[] for i in range(0,len(s)):     c=0     for j in range(0,len(s)):         #checking if two characters are equal         if(s[i]==s[j] and i!=j):             c=1             break     if(c==0):         a.append(s[i]) a=sorted(a) l=[] for i in a:     if ord(i)>90:         l.append(i) print(len(l))

---
+++ count_unique_characters.py
@@ -0,0 +1,17 @@
+s=input()
+a=[]
+for i in range(0,len(s)):
+    c=0
+    for j in range(0,len(s)):
+        #checking if two characters are equal
+        if(s[i]==s[j] and i!=j):
+            c=1
+            break
+    if(c==0):
+        a.append(s[i])
+a=sorted(a)
+l=[]
+for i in a:
+    if ord(i)>90:
+        l.append(i)
+print(len(l))
